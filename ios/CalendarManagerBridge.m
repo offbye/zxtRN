ba283716e46bec598639a91621b2332276fbdc06
张西涛 <zhangxitao@qianmi.com>
2016-06-02 14:58:13 +0800

    fix nonnull error

--- ios/CalendarManagerBridge.m
+++ ios/CalendarManagerBridge.m
@@ -13,6 +13,6 @@
 
 @interface RCT_EXTERN_MODULE(CalendarManager, NSObject)
 
-RCT_EXTERN_METHOD(addEvent:(NSString *)name location:(NSString *)location date:(NSNumber *)date)
+RCT_EXTERN_METHOD(addEvent:(NSString *)name location:(NSString *)location date:nonnull (NSNumber *)date)
 
 @end
